717beb61156345126120a3f167a15a8ff0f767a1
William Stiller <wxs5242@psu.edu>
2023-04-17 21:23:33 -0400

    Adding xml, adding svg exercise1, prepwork for pythonproject

---
+++ projects/pythontest/lotr-files-stage4.py
@@ -0,0 +1,165 @@
+# STAGE 4: CAN WE MAP THE NLP LABELS INTO THE MARKUP AS ATTRIBUTE VALUES?
+# pip install saxonche
+# The library above lets you read and pull data with XPath
+import os
+import spacy
+import re as regex
+from saxonche import PySaxonProcessor
+# ebb: The line above imports the PySaxonProcessor from SaxonCHE (free "home edition")
+# for work with XPath
+
+
+# nlp = spacy.cli.download("en_core_web_lg")
+nlp = spacy.load('en_core_web_lg')
+# ebb: In the line above I'm loading one of the spaCy language models: use either _md or _lg.
+# If you change versions, you need to uncomment the line above and import it, and it can take
+# a little while to finish importing.
+# LOTR PROJECT NOTE: When I ran this with the _md language model, Frodo was being tagged as a PRODUCT.
+# The _lg model seemed more accurate (Frodo as PERSON)
+
+# workingDir = os.getcwd()
+# print("source-txt" + workingDir)
+#
+# insideDir = os.listdir(workingDir)
+# print(str(insideDir))
+#
+# CollPath = os.path.join(workingDir, '../source-xml')
+# print(CollPath)
+###############################################################################
+################################################################################
+# 1. ebb: CollPath can also simply be defined more simply as a relative path
+# defined from this Python file's location, like this, because you climb up one directory
+# and then down into your source XML files:
+##################################################################################
+CollPath = '../source-xml'
+TargetPath = '../taggedWithAtts-TooMany'
+
+# 3. Here, the function imports each individual file, one at a time
+# (received from the for-loop below.
+def readTextFiles(filepath):
+    # with open(filepath, 'r', encoding='utf8') as f:
+    with PySaxonProcessor(license=False) as proc:
+        xml = open(filepath, encoding='utf-8').read()
+        # ebb: Here we changed to the Saxon processor to read files with XPath.
+        # From here on, we change how we formulate the string that Python will send to NLP.
+        xp = proc.new_xpath_processor()
+        node = proc.parse_xml(xml_text=xml)
+        xp.set_context(xdm_item=node)
+
+        xpath = xp.evaluate('//book//p ! normalize-space() => string-join()')
+        # ebb: Let's get the string() value of all the <p> elements that are descendants of <book>.
+        # The XPath function normalize-space() gets the string value and removes extra spaces.
+        # That way we avoid the prologue, preface material.
+        # I'm sending the whole thing to string-join() to bundle it together as one string
+        # instead of a new string for every <p> element.
+        # string = xpath.__str__()
+        string = str(xpath)
+
+        # ebb: I noticed some sentences didn't have a space between end punctuation and the first word of the next sentence.
+        cleanedUp = regex.sub("(\.)([A-Z']])", "\1 \2", string)
+        cleanedUp = regex.sub("'", ' ', cleanedUp)
+        # NOT REALLY DOING THIS, but just for illustration if we want to continue refining the input in multiple stages:
+        # cleanedUp = regex.sub("(\d)", "NUMBER", cleanedUp)
+        # print(cleanedUp)
+        tokens = nlp(cleanedUp)
+        # print(tokens)
+        dictEntities = entitycollector(tokens)
+        # ebb: The line above sends our nlp tokens to the named entity collector function.
+        # THIS current function will receive and print a simple form of their output in the next line.
+        print(f"{dictEntities=}")
+        return(dictEntities)
+
+#########################################################################################
+# ebb: NEXT AFTER RETURNING ALL THE ENTITIES
+# Remove duplicates (get the distinct values of the list of entities
+# Output this information in a useful way
+# Map it back into the XML files
+# GO LOOK AT 2. and 5. below: functions involved: assemblAllNames and xmlTagger
+##########################################################################################
+
+# 4. ebb: The function below returns a simple list of named entities.
+# But on the way, we're printing out as much we can from spaCy's classification of named entities:
+def entitycollector(tokens):
+    with open('output.txt', 'w') as f:
+    # ebb: output.txt is not super useful. It will be full of repetitive information, developed and overwritten as each file in the input collection is processed.
+    # We probably don't really need to write this out as a file. I'm writing it out for illustration purposes only.
+    # The more useful output will be the result of the sorted dictionary of all the entries across the whole collection.
+        entities = {}
+        for ent in sorted(tokens.ents):
+        # if entity.label_ == "NORP" or entity.label_ == "LOC" or entity.label_=="GPE":
+        # ebb: The line helps experiment with different spaCy named entity classifiers, in combination if you like:
+        # When using it, remember to indent the next lines for the for loop.
+            # print(entity.text, entity.label_, spacy.explain(entity.label_))
+            entityInfo = [ent.text, ent.label_, spacy.explain(ent.label_)]
+            stringify = (ent.text + ': ' + ent.label_ + ': ' + spacy.explain(ent.label_) + '\n')
+            # stringify is a string-formatted version of this designed to provide an easily readable file output.
+            print(stringify)
+            f.write(stringify)
+            f.write('\n')
+            entities[ent.text] = ent.label_
+        print(f"{entities=}")
+        return entities
+    # ebb: Keep the return line in position at same indentation level as the definition of the entities variable.
+
+
+# 2. and 5. ebb: The for loop below is working with your CollPath, and going through each file inside,
+# and sending it up to readTextFiles, where the nlp processing will happen.
+def assembleAllNames(CollPath):
+    AllNames = {}
+    for file in os.listdir(CollPath):
+        if file.endswith(".xml"):
+            filepath = f"{CollPath}/{file}"
+            # print(filepath)
+            # print(readTextFiles(filepath))
+            eachFileDict = readTextFiles(filepath)
+            print(f"{eachFileDict=}")
+            # AllNames.append(eachFileDict)
+            AllNames.update(eachFileDict)
+    print(f"{AllNames=}")
+    AllNamesKeys = list(AllNames.keys())
+    AllNamesKeys.sort()
+    SortedDict =  {i: AllNames[i] for i in AllNamesKeys}
+    print(f"{SortedDict=}")
+    writeSortedEntries(SortedDict)
+        # ebb: I'm defining a function that will just write out these entries
+        # line by line in an output file so the sorted dictionary items are easier
+        # to read and review. The file converts the dictionary entries to strings.
+
+    # ebb: FINALLY, let's tag these distinct names in our source XML
+    for file in os.listdir(CollPath):
+        if file.endswith(".xml"):
+            sourcePath = f"{CollPath}/{file}"
+            eachFileData = xmlTagger(sourcePath, SortedDict)
+
+    return eachFileData
+
+def writeSortedEntries(dictionary):
+    with open('distNamesDict.txt', 'w') as f:
+        for key, value in dictionary.items():
+            f.write(key + ' : ' + value + '\n')
+def xmlTagger(sourcePath, SortedDict):
+    with open(sourcePath, 'r', encoding='utf8') as f:
+        readFile = f.read()
+        stringFile = str(readFile)
+
+        # ebb: Get the current filename. We need to know it to write its new output version
+        filename = os.path.basename(f.name)
+        print(f"{filename=}")
+        targetFile = f"{TargetPath}/{filename}"
+        print(f"{targetFile=}")
+
+        # ebb: Work with stringFile variable to look for matches from the distinctNames set.
+        for key, val in SortedDict.items():
+            replacement = '<name type="' + val + '">' + key + '</name>'
+            # print(f"{replacement=}")
+            stringFile = stringFile.replace(key, replacement)
+            # print(f"{stringFile=}")
+
+        # ebb: Output goes in the taggedOutput directory: ../taggedOutput
+        with open(targetFile, 'w') as f:
+            f.write(stringFile)
+
+assembleAllNames(CollPath)
+
+# ebb: The functions are all initiated here now.
+# This just delivers the collection path up to the first function in the sequence.
